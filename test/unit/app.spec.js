998ea62f05501b834f156db3eba8a182719efc48
MateuszC100 <mateuszcierniak102@o2.pl>
2016-05-11 11:10:20 +0200

    fix errors

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -3,7 +3,7 @@ describe('app', function () {
 
     var app = window.app;
 
-    describe("assignToSwimmingCourse", function () {
+    describe('assignToSwimmingCourse', function () {
         it('should assign adult person to adult group', function () {
             expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
                 name: 'Jack', age: 20, course: 'adults'
@@ -11,10 +11,10 @@ describe('app', function () {
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 15, course: 'teens'
+                name: 'John', age: 14, course: 'teens'
             });
         });
-        xit('should assign person below 12 to kids group', function () {
+        it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
             });});
@@ -23,9 +23,9 @@ describe('app', function () {
 				name: 'Zorg', age: 10, course: 'kids'
 			});
 		});
-		xit('should assign person below 12 to kids group', function () {
+		it('should assign person below 12 to kids group', function () {
 			expect(app.assignToSwimmingCourse('John', '10/06/2006')).toEqual({
-				name: 'John', age: 9, course: 'adults'
+				name: 'John', age: 9, course: 'kids'
 			});
 		});
     });
@@ -41,8 +41,10 @@ describe('app', function () {
 				area: 0,message: 'Big null'
 			});
 		});
-		it("check ok", function () {
-			expect(app.calculateArea(2,1,1,'OK','Fail')).toEqual({area: 1 ,message: 'OK'});
+		it('check ok', function () {
+			expect(app.calculateArea(2,1,1,'OK','Fail')).toEqual({
+                area: 1 ,message: 'OK'
+            });
 		});
 	});
 });
